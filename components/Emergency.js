c689acaeb7e3692f208e3b27d007e08c202022f2
Hussein Omar <husseinomar6190@gmail.com>
2022-08-11 19:21:31 +0300

    adding the skeleton code for the screens

---
+++ components/Emergency.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default function Emergency() {
+  return (
+    <View>
+      <Text> Emergency Screen </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
