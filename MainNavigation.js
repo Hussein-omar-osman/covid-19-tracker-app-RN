ef4d5dc89c3659f0c17467d221216b2ac5dec4b6
Hussein Omar <husseinomar6190@gmail.com>
2022-08-11 20:33:53 +0300

    adding icons at the bottom tabs

--- MainNavigation.js
+++ MainNavigation.js
@@ -3,15 +3,46 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
 import Emergency from './components/Emergency';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 export default function MainNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}></Tab.Screen>
-        <Tab.Screen name='Emergency' component={Emergency}></Tab.Screen>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: 'white',
+          tabBarStyle: { backgroundColor: '#3E4F7A' },
+        }}
+      >
+        <Tab.Screen
+          name='Home'
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name='home'
+                size={30}
+                color={focused ? 'white' : '#BEBEBE'}
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name='Emergency'
+          component={Emergency}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name='ambulance'
+                size={30}
+                color={focused ? 'white' : '#BEBEBE'}
+              />
+            ),
+          }}
+        ></Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   );
