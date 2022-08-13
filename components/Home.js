04bd1d474707a9978c1bba634df43a53929f003a
Hussein Omar <husseinomar6190@gmail.com>
2022-08-14 00:29:00 +0300

    styling the search input

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TextInput,
+  Keyboard,
+  Image,
+} from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text> Home Screen </Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder='Search country...'
+            placeholderTextColor='lightgrey'
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#2f3a64',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  input: {
+    backgroundColor: '#3e4f7a',
+    height: 50,
+    borderRadius: 15,
+    paddingLeft: 20,
+    fontSize: 18,
+    width: '80%',
+    marginRight: 10,
+    color: 'lightgrey',
+  },
+});
